af7f1977bd5f11b1c5ad08d10a6f1b4f8cafb51b
itsjared501 <jared.lee501@campus.mccd.edu>
2025-04-30 00:16:14 +0000

    selectClass edit

--- Creature.java
+++ Creature.java
@@ -41,13 +41,35 @@ public class Creature {
 
     /*public ArrayList<Treasure> getInventory() {
         return inventory;
-    }
-
-    public selectClass {
-
-    }
-
-    public selectRace {
-        
     }*/
+
+    public selectClass() {
+        //display all classes and their abilities
+        //ask the user to select a class
+        System.out.println("Classes -");
+        System.out.println("1. Cleric");
+        System.out.println("");
+        System.out.println("");
+        System.out.println(" ");
+        System.out.println("2. Thief");
+        System.out.println("Expertise:   You gain an extra treasure when looting the room.");
+        System.out.println("Slippery Feet:   You gain +1 to run away.");
+        System.out.println(" ");
+        System.out.println("3. Warrior");
+        System.out.println("Hard-Hitter:   You gain +1 to attack power.");
+        System.out.println("Duelist:   You now win ties in combat.");
+        System.out.println(" ");
+        System.out.println("4. Wizard");
+        System.out.println("Charm:   While in combat, you may discard your entire inventory to automatically beat the monster. You obtain all treasures but do not level up.");
+        System.out.println("Potion Master:   ");
+        System.out.println(" ");
+        System.out.println("Select a class by entering the number of the class you want to select.");
+        String classSelection = scanner.nextLine();
+        userClass = classSelection;
+    }
+
+    public selectRace() {
+        //display all races and their advantages and disadvantages
+        //ask the user to select a race
+    }
 }
--- Game.java
+++ Game.java
@@ -1,21 +1,29 @@
+//Jared Lee
+//April 29, 2025
+//This is a text-based RPG game based on the card game Munchkin.
 import java.util.Scanner;
 //import java.util.ArrayList;
 
 class Game {
-
+    //This is the main class for the game. It will handle the game loop and user input.
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //Introduce the user to the game and ask for their information.
         System.out.println("Welcome to the epicness that is Munchkin! To start off your journey, tell me about yourself!");
         System.out.print("What is your name? ");
-        String name = scanner.nextLine();
+        String name = scanner.nextLine(); //this will be used to create the user object of the class Creature
 
-        System.out.print("What is your gender? ");
-        String gender = scanner.nextLine();
+        System.out.print("What is your gender? Male/Female");
+        String gender = scanner.nextLine(); //this will be used to create the user object of the class Creature
 
-        Creature user = new Creature(name, gender);
+        Creature user = new Creature(name, gender); //uses the constructor to create the user object
 
-        System.out.println("Wow, " + user.getName() + "! What a great name! Now, what class do you want to be?");
+        System.out.println("Wow, " + user.getName() + "! What an eh name... It's no Godfrey the Slayer of Fae! Or Beyonce the Queen Bee! Now, what race are you?");
+
+        //bring up the selectRace method to allow the user to select one
+
+        //bring up the selectClass method to allow the user to select one
 
         scanner.close();
     }
