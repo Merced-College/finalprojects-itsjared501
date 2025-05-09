d1ebe95aa084ba652e860890b02fea942361628a
itsjared501 <jared.lee501@campus.mccd.edu>
2025-05-09 19:46:42 +0000

    deleted useless imports

--- Game.java
+++ Game.java
@@ -4,13 +4,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
 import java.util.Scanner;
 import java.util.HashMap;
-//import java.util.LinkedList;
 import java.util.Map;
 import java.util.Random;
 
